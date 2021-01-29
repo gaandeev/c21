0923c1b3906f9238647163c70453bf30b6c74eea
gaandeev <77530799+gaandeev@users.noreply.github.com>
2021-01-29 16:52:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var fixedRect, movingRect;
+var car,wall;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+  car=createSprite(200,200,40,40);
+  car.shapeColor = "blue";
+  car.velocityX=4;
+  wall=createSprite(1000,200,20,100);
+  wall.shapeColor = "orange";
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+  bounceOff(car,wall);
+
+  if(isTouching(wall,movingRect)){
+ text("touched",200,200);
+ movingRect.shapeColor="pink";
+ wall.shapeColor="pink";
+  }
+  drawSprites();
+}
+function isTouching(object1,object2){
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2
+    && object1.y - object2.y < object2.height/2 + object1.height/2
+    && object2.y - object1.y < object2.height/2 + object1.height/2) {
+ return true;
+}
+else {
+ return false;
+}
+}
+function bounceOff(object1,object2){
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2){
+ object1.velocityX=object1.velocityX*(-1);
+ object2.velocityX=object2.velocityX*(-1);
+    } 
+    else if (object1.y - object2.y < object2.height/2 + object1.height/2
+    && object2.y - object1.y < object2.height/2 + object1.height/2){
+      object1.velocityY=object1.velocityY*(-1);
+      object2.velocityY=object2.velocityY*(-1);
+    }
+}
